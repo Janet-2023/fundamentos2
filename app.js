4e0b77c2f819f03974a8a6a23357875d7d9450e7
Janet <nope.janet.97@gmail.com>
2023-04-26 22:08:01 -0500

    actualizando

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
 
 //suma
+
+/*
 let number1 = 10;
 let number2 = 20;
 
+let suma = number1 + number2;
+let resta = number1 - number2;
+let multiplicacion = number1 * number2;
+let division = number1 / number2;
+*/
 
 // modulo
 
-let modulo = number2;
+//let modulo = number1 % number1;
+
+// console.log(suma);
+//console.log(resta);
+//console.log(division);
+//console.log(modulo)
+
+// if / else
+/*
+let edad = 19;
+
+if(edad> 18){
+    console.log('Eres mayor de edad');
+    }else{
+        console.log('Eres mennos de edad');
+    }
 
 
-const año = prompt ('Ingrese el año de nacimiento')
+const año = prompt ('Ingrese el año de  tu nacimiento')
 console.log(año-2023)
+*/
+
+//OPERACIONES DE COMPARACION
+// mayor , menor 
+
+
+/*
+function devuelveDoble(number){
+    return number = 2;
+}
+console.log(devuelveDoble(20))
+
+*/
+
+let number1 = 20;
+let number2 = 10;
+let resta = number1 - number2;
+let suma = number1 + number2;
+let division = number1 / number2;
+let multiplicacion = number1 * number2;
+
+
+
+console.log(resta, division)
